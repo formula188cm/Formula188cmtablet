65e95626064f44d80ff731279c3e95d54733e1be
formula188cm <formula188cm@gmail.com>
2025-06-14 14:26:15 +0530

    Update payment.js

--- payment.js
+++ payment.js
@@ -34,10 +34,10 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Define Razorpay payment links for different quantities
     const paymentLinks = {
-        1: "https://rzp.io/rzp/TWF5xmG1",  // Link for quantity 1 (₹1,299)
-        2: "https://rzp.io/rzp/fDZQCst",   // Link for quantity 2 (₹2,598)
-        3: "https://rzp.io/rzp/XfMvJv2",   // Link for quantity 3 (₹3,897)
-        4: "https://rzp.io/rzp/OHLRJvIc"   // Link for quantity 4 (₹5,196)
+        1: "https://rzp.io/rzp/ZZ7hEzR",  // Link for quantity 1 (₹1,299)
+        2: "https://rzp.io/rzp/QusxuLhM",   // Link for quantity 2 (₹2,598)
+        3: "https://rzp.io/rzp/m5yCXJ5H",   // Link for quantity 3 (₹3,897)
+        4: "https://rzp.io/rzp/UuxPG3hK"   // Link for quantity 4 (₹5,196)
     };
 
     // Payment method selection
